03769c7983f2670e085aa2b322544f314f98d642
nickzmoro <nicolasmororuiz@gmail.com>
2025-05-05 23:01:22 -0300

    style: mobile menu structure added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,47 +1,82 @@
-import { HeaderBox, NavList, List } from "./styles.js";
-import { HiUser } from "react-icons/hi";
-import { Link, useLocation } from 'react-router-dom'
+import { HeaderBox, NavList, List, MobileMenu } from "./styles.js";
+import { HiUser, HiOutlineMenuAlt3 } from "react-icons/hi";
+import { Link, useLocation } from "react-router-dom";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 export function Header() {
+  const { pathname } = useLocation();
+  const [changeBackground, setChangeBackground] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
-    const { pathname } = useLocation();
-    const [ changeBackground, setChangeBackground ] = useState(false)
-
-    window.onscroll = () => {
-        if(window.scrollY > 80) {
-            setChangeBackground(true)
-        } else {
-            setChangeBackground(false)
-        }
+  window.onscroll = () => {
+    if (window.scrollY > 80) {
+      setChangeBackground(true);
+    } else {
+      setChangeBackground(false);
     }
+  };
 
-    return (
-        <HeaderBox changeBackground={changeBackground}>
-            <h2>Dev<span>flix</span></h2>
-            <NavList>
-                <ul>
-                    <List isActive={pathname === '/'}>
-                        <Link to='/'>Home</Link>
-                    </List>
-                    <List isActive={pathname.includes('filmes')}>
-                        <Link to='/filmes'>Filmes</Link>
-                    </List>
-                    <List isActive={pathname.includes('series')}>
-                        <Link to='/series'>Séries</Link>
-                    </List>
-                </ul>
-                <button id="login">
-                    <div className="container-icon">
-                        <HiUser className="icon" />
-                    </div>
-                    <div className="login-txt">
-                        <p>Faça Login</p>
-                        <span>ou cadastre-se</span>
-                    </div>
-                </button>
-            </NavList>
-        </HeaderBox>
-    )
+  return (
+    <HeaderBox changeBackground={changeBackground}>
+      <h2>
+        Dev<span>flix</span>
+      </h2>
+      <NavList>
+        <ul>
+          <List isActive={pathname === "/"}>
+            <Link to="/">Home</Link>
+          </List>
+          <List isActive={pathname.includes("filmes")}>
+            <Link to="/filmes">Filmes</Link>
+          </List>
+          <List isActive={pathname.includes("series")}>
+            <Link to="/series">Séries</Link>
+          </List>
+        </ul>
+        <button id="login">
+          <div className="container-icon">
+            <HiUser className="icon" />
+          </div>
+          <div className="login-txt">
+            <p>Faça Login</p>
+            <span>ou cadastre-se</span>
+          </div>
+        </button>
+      </NavList>
+      <MobileMenu>
+        <button
+          className="btn-menu-open"
+          onClick={() => setShowMobileMenu(true)}
+        >
+          <HiOutlineMenuAlt3 size={30} />
+        </button>
+        {showMobileMenu && (
+          <>
+            <div>
+              <button
+                className="btn-menu-close"
+                onClick={() => setShowMobileMenu(false)}
+              >
+                Close
+              </button>
+            </div>
+            <div className="menu-mobile">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/filmes">Filmes</Link>
+                </li>
+                <li>
+                  <Link to="/series">Séries</Link>
+                </li>
+              </ul>
+            </div>
+          </>
+        )}
+      </MobileMenu>
+    </HeaderBox>
+  );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,105 +1,136 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const HeaderBox = styled.header`
-    position: fixed;
-    top: 0;
-    z-index: 99;
-    width: 100%;
-    height: 10vh;
-    display: flex;
-    align-items: center;
-    padding: 0 100px;
-    justify-content: space-between;
-    transition: 0.2s ease;
-    background-color: ${(props) => (props.changeBackground ? '#000' : 'transparent')};
-    border-bottom: ${(props) => (props.changeBackground ? '1px solid rgba(256, 256, 256, 0.1)' : 'none')};
+  position: fixed;
+  top: 0;
+  z-index: 99;
+  width: 100%;
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  padding: 0 100px;
+  justify-content: space-between;
+  transition: 0.2s ease;
+  background-color: ${(props) =>
+    props.changeBackground ? "#000" : "transparent"};
+  border-bottom: ${(props) =>
+    props.changeBackground ? "1px solid rgba(256, 256, 256, 0.1)" : "none"};
 
-    & h2 {
-        text-transform: uppercase;
-        font-weight: 900;
-        font-size: 2.2rem;
+  @media (max-width: 845px) {
+    padding: 0 2.5rem;
+  }
 
-        span {
-            color: red;
-        }
+  & h2 {
+    text-transform: uppercase;
+    font-weight: 900;
+    font-size: 2.2rem;
+
+    span {
+      color: red;
     }
-`
+  }
+`;
 
 export const NavList = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  @media (max-width: 845px) {
+    display: none;
+  }
+
+  ul {
     display: flex;
+    gap: 40px;
+    margin-right: 20px;
+  }
+
+  #login {
+    display: flex;
+    background-color: #ff0000;
+    border: none;
+    border-radius: 15px;
+    padding: 8px 12px;
+    cursor: pointer;
+    gap: 8px;
     align-items: center;
-    gap: 20px;
+    border: 1px solid #ff0000;
+    transition: 0.1s ease;
 
-    ul {
-        display: flex;
-        gap: 40px;
-        margin-right: 20px;
+    &:hover {
+      filter: saturate(75%);
+      border: 1px solid rgba(256, 256, 256, 0.2);
     }
 
-    #login {
-        display: flex;
-        background-color: #FF0000;
-        border: none;
-        border-radius: 15px;
-        padding: 8px 12px;
-        cursor: pointer;
-        gap: 8px;
-        align-items: center;
-        border: 1px solid #FF0000;
-        transition: 0.1s ease;
-
-        &:hover {
-            filter: saturate(75%);
-            border: 1px solid rgba(256, 256, 256, 0.2);
-        }
-
-        .container-icon {
-            padding: 8px 8px;
-            background-color: rgba(0, 0, 0, 0.2);
-            border-radius: 50px;
-            display: flex;
-            align-items: center;
-        }
-
-        .login-txt {
-            p {
-                font-size: 1.1rem;
-                font-weight: 600;
-                text-align: start;
-            }
-
-            span {
-                font-size: 0.9rem;
-                color: rgba(256, 256, 256, 0.6);
-            }
-        }
+    .container-icon {
+      padding: 8px 8px;
+      background-color: rgba(0, 0, 0, 0.2);
+      border-radius: 50px;
+      display: flex;
+      align-items: center;
     }
-`
+
+    .login-txt {
+      p {
+        font-size: 1.1rem;
+        font-weight: 600;
+        text-align: start;
+      }
+
+      span {
+        font-size: 0.9rem;
+        color: rgba(256, 256, 256, 0.6);
+      }
+    }
+  }
+`;
 
 export const List = styled.li`
-    position: relative;
+  position: relative;
 
-    &::after {
-        content: ' ';
-        height: 2px;
-        width: ${(props) => (props.isActive ? '100%' : 0)};
-        background-color: #FF0000;
-        position: absolute;
-        bottom: -5px;
-        left: 0;
-        border-radius: 50px;
-        transition: 0.2s ease;
-    }
+  &::after {
+    content: " ";
+    height: 2px;
+    width: ${(props) => (props.isActive ? "100%" : 0)};
+    background-color: #ff0000;
+    position: absolute;
+    bottom: -5px;
+    left: 0;
+    border-radius: 50px;
+    transition: 0.2s ease;
+  }
 
-    &:hover::after {
-        width: 100%;
-        left: 1;
-    }
+  &:hover::after {
+    width: 100%;
+    left: 1;
+  }
 
-    a {
-        font-size: 1.2rem;
-        font-weight: 600;
-        border-radius: 5px;
-        transition: 0.2s ease;
-    }
-`
+  a {
+    font-size: 1.2rem;
+    font-weight: 600;
+    border-radius: 5px;
+    transition: 0.2s ease;
+  }
+`;
+
+export const MobileMenu = styled.div`
+  display: none;
+
+  .line-menu {
+    width: 30px;
+    height: 2px;
+    background-color: #fff;
+    margin: 3px 0;
+  }
+
+  .btn-menu-open {
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  @media (max-width: 845px) {
+    display: block;
+  }
+`;
